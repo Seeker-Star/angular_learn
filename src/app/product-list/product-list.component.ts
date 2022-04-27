af330e2a7e3b528bdeb47de9b9114b62f62ea970
xuyataobj <xuyataobj@yuanfudao.com>
2022-04-27 19:37:16 +0800

    feat: 事件绑定

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+  public title = '我是 title 属性';
+  public styleProperty = '<b>我是包含 html 标签的属性</b>';
+  public fontColor = 'red';
+  public url = 'https://google.com.hk';
+  public name: string | undefined;
+  public msg: string | undefined
+  public refMsg: string | undefined
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  getUser() {
+    alert(this.name)
+  }
+
+  getMsg(event: KeyboardEvent) {
+    console.log(event);
+    this.msg = (event.target as HTMLInputElement).value;
+  }
+
+  getRefMsg(msg: string) {
+    this.refMsg = msg
+  }
+}
